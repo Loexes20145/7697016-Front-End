80ad6aa2199853fb39d07f7bd7e7409aca23eda5
loexes20145 <emaklov8145@gmail.com>
2024-10-21 15:59:40 +0000

    modified:   index.html 	modified:   pieces.js

--- pieces.js
+++ pieces.js
@@ -2,7 +2,7 @@
 const reponse = await fetch("pieces-autos.json")
 const pieces = await reponse.json()
 
-// Création des balises
+// Création des articles de pieces
 for (let article of pieces) {
     const imageElement = document.createElement("img")
     imageElement.src = article.image
@@ -29,3 +29,40 @@ for (let article of pieces) {
 
     sectionFiches.appendChild(articleElement)
 }
+
+// Gestion des boutons
+const boutonTrierCroissant = document.querySelector(".btn-trier-croissant")
+
+boutonTrierCroissant.addEventListener("click", function () {
+    const piecesOrdonnees = Array.from(pieces)
+    piecesOrdonnees.sort(function (a, b) {
+        return a.prix - b.prix
+    })
+    console.log(piecesOrdonnees)
+})
+
+const boutonTrierDecroissant = document.querySelector(".btn-trier-decroissant")
+
+boutonTrierDecroissant.addEventListener("click", function() {
+    const piecesOrdonnees = Array.from(pieces)
+    piecesOrdonnees.sort(function (a, b) {
+        return b.prix - a.prix
+    })
+    console.log(piecesOrdonnees)
+})
+
+const boutonFiltrerCheap = document.querySelector(".btn-filtrer-cheap")
+boutonFiltrerCheap.addEventListener("click", function () {
+    const piecesFiltrees = pieces.filter (function (piece) {
+        return piece.prix <= 35
+    })
+    console.log(piecesFiltrees)
+})
+
+const boutonNoDescription = document.querySelector(".btn-filtrer-nodesc")
+boutonNoDescription.addEventListener("click", function () {
+    const piecesFiltrees = pieces.filter(function (piece) {
+        return piece.description
+    })
+    console.log(piecesFiltrees)
+})
