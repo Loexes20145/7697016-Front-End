aa1cbf2c03a57d286dbfbfaa133269547bc23aaf
loexes20145 <emaklov8145@gmail.com>
2024-10-21 14:37:10 +0000

    modified:   pieces.js 	modified:   styles.css

--- pieces.js
+++ pieces.js
@@ -3,25 +3,29 @@ const reponse = await fetch("pieces-autos.json")
 const pieces = await reponse.json()
 
 // Création des balises
-const article = pieces[0]
-const imageElement = document.createElement("img")
-imageElement.src = article.image
-const nomElement = document.createElement("h2")
-nomElement.innerText = article.nom
-const prixElement = document.createElement("p")
-prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`
-const categorieElement = document.createElement("p")
-categorieElement.innerText = article.categorie ?? "* Pas de catégorie"
-const descriptionElement = document.createElement("p")
-descriptionElement.innerText = `${article.description ?? "*Pas de description pour le moment"}`
-const stockElement = document.createElement("p")
-stockElement.innerText = article.disponibilite ? "En stock" : "En rupture de stock"
+for (let article of pieces) {
+    const imageElement = document.createElement("img")
+    imageElement.src = article.image
+    const nomElement = document.createElement("h2")
+    nomElement.innerText = article.nom
+    const prixElement = document.createElement("p")
+    prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`
+    const categorieElement = document.createElement("p")
+    categorieElement.innerText = article.categorie ?? "* Pas de catégorie"
+    const descriptionElement = document.createElement("p")
+    descriptionElement.innerText = `${article.description ?? "*Pas de description pour le moment"}`
+    const stockElement = document.createElement("p")
+    stockElement.innerText = article.disponibilite ? "En stock" : "En rupture de stock"
 
-// Rattachement des balises au DOM
-const sectionFiches = document.querySelector(".fiches")
-sectionFiches.appendChild(imageElement)
-sectionFiches.appendChild(nomElement)
-sectionFiches.appendChild(prixElement)
-sectionFiches.appendChild(categorieElement)
-sectionFiches.appendChild(descriptionElement)
-sectionFiches.appendChild(stockElement)
+    // Rattachement des balises au DOM
+    const sectionFiches = document.querySelector(".fiches")
+    const articleElement = document.createElement("article")
+    articleElement.appendChild(imageElement)
+    articleElement.appendChild(nomElement)
+    articleElement.appendChild(prixElement)
+    articleElement.appendChild(categorieElement)
+    articleElement.appendChild(descriptionElement)
+    articleElement.appendChild(stockElement)
+
+    sectionFiches.appendChild(articleElement)
+}
