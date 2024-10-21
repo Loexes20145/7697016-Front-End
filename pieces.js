30a358958f8411b03fcf473d52dbaef5d8188eef
loexes20145 <emaklov8145@gmail.com>
2024-10-21 17:07:29 +0000

    modified:   index.html 	modified:   pieces.js

--- pieces.js
+++ pieces.js
@@ -66,3 +66,43 @@ boutonNoDescription.addEventListener("click", function () {
     })
     console.log(piecesFiltrees)
 })
+
+// Récupération des noms des pieces
+const noms = pieces.map(piece => piece.nom)
+for (let i = pieces.length-1 ; i >= 0 ; i--) {
+    if (pieces[i].prix > 35) {
+        noms.splice(i, 1)
+    }
+}
+
+// Création et affichage de la liste des pieces abordables
+const abordablesElements = document.createElement('ul')
+
+for (let i=0 ; i < noms.length ; i++) {
+    const nomElement = document.createElement('li')
+    nomElement.innerText = noms[i]
+    abordablesElements.appendChild(nomElement)
+}
+
+document.querySelector(".abordables").appendChild(abordablesElements)
+
+// Création et affichage de la liste des pièces disponibles
+const nomsDisponibles = pieces.map(piece => piece.nom)
+const prixDisponibles = pieces.map(piece => piece.prix)
+
+for (let i = nomsDisponibles.length-1 ; i >= 0 ; i--) {
+    if (!pieces[i].disponibilite) {
+        nomsDisponibles.splice(i, 1)
+        prixDisponibles.splice(i, 1)
+    }
+}
+
+const disponiblesElement = document.createElement('ul')
+
+for (let i=0 ; i < nomsDisponibles.length ; i++) {
+    const nomElement = document.createElement('li')
+    nomElement.innerText = `${nomsDisponibles[i]} - ${prixDisponibles[i]} €`
+    disponiblesElement.appendChild(nomElement)
+}
+
+document.querySelector('.disponibles').appendChild(disponiblesElement)
